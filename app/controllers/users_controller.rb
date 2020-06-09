1cfb94d114cf5ce2927afc73122e95f22ba42701
yodev <yodev2177@gmail.com>
2020-06-09 15:15:00 +0900

    Add 退会機能実装 #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
-  
+  before_action :set_params, only: [:show, :destroy]
   def index
     @users = User.all
   end
 
-  def show
+  def show; end
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_registration_path, notice: "退会しました"
+  end
+
+  private
+  def set_params
     @user = User.find(params[:id])
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: { registrations: 'registrations' }
   root to: "teams#index"
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
   resources :teams, only: [:index]
 end
