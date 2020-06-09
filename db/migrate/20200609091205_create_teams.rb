acd445430cb417f6cbb0e8b6791a0158b9b03064
yodev <yodev2177@gmail.com>
2020-06-09 18:20:42 +0900

    Add チームテーブルのマイグレーション実行 #6

--- db/migrate/20200609091205_create_teams.rb
+++ db/migrate/20200609091205_create_teams.rb
@@ -1,6 +1,10 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
+      t.string :name, null: false
+      t.text :image, null: false
+      t.text :remarks
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_051029) do
+ActiveRecord::Schema.define(version: 2020_06_09_091205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "image", null: false
+    t.text "remarks"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_06_09_051029) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "teams", "users"
 end
