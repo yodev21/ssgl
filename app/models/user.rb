1c8f5ceb72e9217ceb0e70f62cc18aa6ec6a7225
yodev <yodev2177@gmail.com>
2020-06-09 14:58:32 +0900

    Add プロフィール修正機能実装 #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 50 }
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
   root to: "teams#index"
   resources :users, only: [:index, :show]
   resources :teams, only: [:index]
