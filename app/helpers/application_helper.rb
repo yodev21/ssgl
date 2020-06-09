86f6b1f54b118efe4291e77fd469a965a36e405a
yodev <yodev2177@gmail.com>
2020-06-09 14:35:03 +0900

    Add プロフィール画面作成 #1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(user)
+    return user.image unless user.image.nil?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_023646) do
+ActiveRecord::Schema.define(version: 2020_06_09_051029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_023646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
+    t.text "profile"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
