1533358dbe27413b857374ca440cfc0524663a44
yodev <yodev2177@gmail.com>
2020-06-09 22:52:15 +0900

    Add チーム新規登録機能実装 #6

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,30 @@
 class TeamsController < ApplicationController
-  def index
-    
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
+  def new
+    @team = Team.new
   end
+
+  def create
+    @team = current_user.teams.build(team_params)
+    if @team.save
+      redirect_to teams_path, notice: "チームを作成しました！"
+    else
+      flash.now[:danger] = "チームの作成に失敗しました！"
+      render :new
+    end
+  end
+
+  def index
+    @teams = Team.all.order(created_at: :desc)
+  end
+
+  private
+  def set_params
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name,:image,:remarks)
+  end
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,8 @@
 class Team < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :image, presence: true
+  
+  mount_uploader :image, ImageUploader
 end
