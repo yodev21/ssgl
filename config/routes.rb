83d909dc83279d3ab7bc874313de292817038f3c
yodev <yodev2177@gmail.com>
2020-06-10 20:41:09 +0900

    Add tasksコントローラーの自動生成 #10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   devise_for :users,
   controllers: { registrations: 'registrations' }
   root to: "teams#index"
