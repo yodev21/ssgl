a2bab9d0f7a0126935c5a441f4a4d142b4e50e29
yodev <yodev2177@gmail.com>
2020-06-15 21:15:29 +0900

    Add コメントモデル作成 #23

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
+//= require jquery/dist/jquery.js
 //= require_tree .
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class Answer < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :team
   belongs_to :assign
