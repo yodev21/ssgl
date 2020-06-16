6d3516847477c96f2941a9161cd2a57c3a3a88f3
yodev <yodev2177@gmail.com>
2020-06-16 11:11:28 +0900

    Add フィードバックモデル作成 #26

---
+++ db/migrate/20200616020317_create_feed_backs.rb
@@ -0,0 +1,16 @@
+class CreateFeedBacks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feed_backs do |t|
+      t.integer :feeling_numbe, null: false
+      t.text :reason
+      t.references :user, foreign_key: true, index: true, null: false
+      t.references :team, foreign_key: true, index: true, null: false
+      t.references :assign, foreign_key: true, index: true, null: false
+      t.references :task, foreign_key: true, index: true, null: false
+      t.references :challenge_start, foreign_key: true, index: true, null: false
+      t.references :answer, foreign_key: true, index: true, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/feed_backs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :feed_back do
+    
+  end
+end
