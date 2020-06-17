9871665f6ebb4e7fccc3c39d64d18aca9d8fba8c
yodev <yodev2177@gmail.com>
2020-06-17 17:43:50 +0900

    Add ゲストユーザー機能実装 close  #30

---
+++ app/controllers/guest_users_controller.rb
@@ -0,0 +1,9 @@
+class GuestUsersController < ApplicationController
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+end
