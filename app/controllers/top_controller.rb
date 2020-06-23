36b7408edf81a6d5999b5e416da0ec1e52277dcb
yodev <yodev2177@gmail.com>
2020-06-23 23:13:28 +0900

    Fix ログイン時トップページに遷移しないよう修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,13 @@
 class TopController < ApplicationController
+  before_action :login_check
+
   def top
+  end
+
+  private
+  def login_check
+    if user_signed_in?
+      redirect_to teams_path, notice: "ログインしています。"
+    end
   end
 end
