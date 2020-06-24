9dd34fef3c1311ace9664416d50058fe2608ea4d
yodev <yodev2177@gmail.com>
2020-06-25 05:59:12 +0900

    Add クラウドフロント設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'static.yodev21-ssgl.work'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
