140f85b35a240e96be5c0b6da6ccf2b18e333603
yodev <yodev2177@gmail.com>
2020-06-24 14:49:27 +0900

    Add carrierwave 修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,20 +5,20 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                'ap-northeast-1',
-      path_style:            true,
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1',
+    path_style: true
   }
   config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.fog_attributes = { 'Cache-Control' => 'public, max-age=86400' }
   case Rails.env
-    when 'production'
-      config.fog_directory = 'ssgl-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ssgl-production'
-    when 'development'
-      config.fog_directory = 'ssgl-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ssgl-development'
+  when 'production'
+    config.fog_directory = 'ssgl-production'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ssgl-production'
+  when 'development'
+    config.fog_directory = 'ssgl-development'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ssgl-development'
   end
 end
