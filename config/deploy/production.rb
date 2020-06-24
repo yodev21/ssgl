ad6e339666becde6c357a65e3679e464a3f92baf
yodev <yodev2177@gmail.com>
2020-06-25 04:51:30 +0900

    Add 元に戻す

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,2 @@
-# server '18.180.228.184', user: 'app', roles: %w{app db web}
-server 'yodev21-ssgl.work', user: 'app', roles: %w{app db web}
+server '18.180.228.184', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
