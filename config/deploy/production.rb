e38d618c64d830f1452cb07187359676c92a30a0
yodev <yodev2177@gmail.com>
2020-06-25 04:41:37 +0900

    Add production修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-server '18.180.228.184', user: 'app', roles: %w{app db web}
+# server '18.180.228.184', user: 'app', roles: %w{app db web}
+server 'yodev21-ssgl.work', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
