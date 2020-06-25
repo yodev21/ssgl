58c8ea3ab046a2fcd8fd7f3a1f539a070e9d8878
yodev <yodev2177@gmail.com>
2020-06-26 02:55:22 +0900

    Test チャレンジタスクのシステムテストコード記述 #59

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  wait = Selenium::WebDriver::Wait.new(:timeout => 100) 
+  wait = Selenium::WebDriver::Wait.new(:timeout => 500) 
   describe "タスク機能" do
     before do
       @user = FactoryBot.create(:user, email: "testTaskSystem@example.com")
@@ -48,30 +48,6 @@ RSpec.describe 'Tasks', type: :system do
       click_link "課題削除"
       page.driver.browser.switch_to.alert.accept
       wait.until{ expect(page).to have_content "削除しました"}
-    end
-
-    example "タスクに取り組むことができること", :retry => 3 do
-      visit new_team_assign_task_path(team_id: @team.id, assign_id: @assign.id)
-      fill_in "task_title", with: "test Title"
-      attach_file 'task_image', "#{Rails.root}/app/assets/images/tasks/1.png"  
-      fill_in "task_content", with: "test Content"
-      click_button "登録"
-      visit team_assign_task_path(team_id: @team.id, assign_id: @assign.id, id: @assign.tasks.first)
-      click_link "取り組む"
-      wait.until{ expect(page).to have_content "こちらの課題に取り組みました"}
-    end
-
-    example "タスクを諦めることができること", :retry => 3 do
-      visit new_team_assign_task_path(team_id: @team.id, assign_id: @assign.id)
-      fill_in "task_title", with: "test Title"
-      attach_file 'task_image', "#{Rails.root}/app/assets/images/tasks/1.png"  
-      fill_in "task_content", with: "test Content"
-      click_button "登録"
-      visit team_assign_task_path(team_id: @team.id, assign_id: @assign.id, id: @assign.tasks.first)
-      click_link "取り組む"
-      click_link "諦める"
-      page.driver.browser.switch_to.alert.accept
-      wait.until{ expect(page).to have_content "こちらの課題を取り消しました"}
     end
 
     example "タスクの検索ができること", :retry => 3 do
