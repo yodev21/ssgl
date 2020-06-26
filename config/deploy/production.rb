38ec9e03adaeb6d5ab93c121dc3603d7aabc4d3b
yodev <yodev2177@gmail.com>
2020-06-26 19:35:41 +0900

    Add オリジンサーバーデプロイ設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # オリジン
-# server '18.180.228.184', user: 'app', roles: %w{app db web}
+server '18.180.228.184', user: 'app', roles: %w{app db web}
 
 #バックアップ
-server '54.64.111.71', user: 'app', roles: %w{app db web}
+# server '54.64.111.71', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
