8a31bbd9f818f0f4d24e35f057cea390c3f18b9b
yodev <yodev2177@gmail.com>
2020-06-26 21:40:08 +0900

    Fix レイアウトによるテストの失敗を修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
-# オリジン
+# SSGL-production
 server '18.180.228.184', user: 'app', roles: %w{app db web}
 
-#バックアップ
+# SSGL-production-ami
 server '54.64.111.71', user: 'app', roles: %w{app db web}
+
 set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
