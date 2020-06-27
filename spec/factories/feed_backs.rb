02b08d126fa6bed040f4e2ba8fcc555fc328bc4d
yodev <yodev2177@gmail.com>
2020-06-27 21:16:00 +0900

    Add フィードバック機能のテスト記述 close #70

--- spec/factories/feed_backs.rb
+++ spec/factories/feed_backs.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :feed_back do
-    
+    feeling_number { 1 }
+    reason { "テスト リーズン" }
+    team_id { 1 }
+    assign_id { 1 }
+    task_id { 1 }
+    challenge_start_id { 1 }
+    answer_id { 1 }
   end
 end
