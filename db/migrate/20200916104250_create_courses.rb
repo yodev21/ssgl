701b99f095c6ac742359248d70adc084c4eeb486
yodev <yodev2177@gmail.com>
2020-09-16 19:47:10 +0900

    コースモデルの自動生成

---
+++ db/migrate/20200916104250_create_courses.rb
@@ -0,0 +1,14 @@
+class CreateCourses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :courses do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.text :image, null: false
+      t.references :user, foreign_key: true, index: true, null: false
+      t.references :team, foreign_key: true, index: true, null: false
+      t.references :assign, foreign_key: true, index: true, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/courses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :course do
+    title "MyString"
+    content "MyString"
+    references ""
+    references ""
+    references "MyString"
+    assign_id "MyString"
+  end
+end
